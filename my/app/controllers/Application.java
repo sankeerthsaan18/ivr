87518e86b31127c08d0ec56aaf37e12394e9d701
saan <swethay.409@gmail.com>
2012-02-05 10:45:34 +0530

    first commit

---
+++ my/app/controllers/Application.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+     public static void coni() {
+        render();
+    }
+public static void fest() {
+        render();
+    }public static void resr() {
+        render();
+    }public static void wedding() {
+        render();
+    }public static void medical() {
+        render();
+    }public static void s(String myName) {
+        render(myName);
+    }public static void signin() {
+        render();
+    }
+}
